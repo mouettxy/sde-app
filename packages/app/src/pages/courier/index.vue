ca3e75f156eb1356ffab9b648df6b751369c32b2
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:51:11 +0300

    feat(courierIndex): init index page

---
+++ packages/app/src/pages/courier/index.vue
@@ -0,0 +1,20 @@
+<route>
+{
+  "name": "courierIndex",
+  "meta": {
+    "header": "Рабочий стол"
+  }
+}
+</route>
+
+<template lang="pug">
+.page-logist-index
+  span page courier index
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class PageCourierIndex extends Vue {}
+</script>

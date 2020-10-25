5e271e102c17d6007499a4663288cf83e1e425f8
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:50:31 +0300

    feat(logistIndex): init logist page

---
+++ packages/app/src/pages/logist/index.vue
@@ -0,0 +1,20 @@
+<route>
+{
+  "name": "logistIndex",
+  "meta": {
+    "header": "Рабочий стол"
+  }
+}
+</route>
+
+<template lang="pug">
+.page-logist-index
+  span page logist index
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class PageLogistIndex extends Vue {}
+</script>

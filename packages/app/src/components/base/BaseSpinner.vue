9a277905b8a2493bd95cd631a2808e953318cf42
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:47:04 +0300

    feat(baseComponents): init base spinner component

---
+++ packages/app/src/components/base/BaseSpinner.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+v-progress-circular(
+  indeterminate,
+  color='black'
+)
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BaseSpinner extends Vue {}
+</script>
+
+<style lang="sass"></style>

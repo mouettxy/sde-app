30a1447cfb4759cf2c3ec5a82139a18473d2270d
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:42:09 +0300

    feat(router): add navigation guards

--- packages/app/src/router/index.ts
+++ packages/app/src/router/index.ts
@@ -1,7 +1,10 @@
+import { authModule } from './../store/index'
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from 'vue-auto-routing'
 import { createRouterLayout } from 'vue-router-layout'
+import { includes } from 'lodash'
+import NProgress from '@/plugins/progress-bar'
 
 Vue.use(Router)
 
@@ -9,7 +12,7 @@ const RouterLayout = createRouterLayout((layout) => {
   return import('@/layouts/' + layout + '.vue')
 })
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -17,4 +20,35 @@ export default new Router({
       children: routes,
     },
   ],
+  mode: 'history',
+})
+
+export default router
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = authModule.isLoggedIn
+
+  const notAuthPages = ['login', 'index', 'order', 'client']
+
+  if ((!isLoggedIn && includes(notAuthPages, to.name)) || isLoggedIn) {
+    next()
+    return
+  } else {
+    next({ name: 'login' })
+    return
+  }
+})
+
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    NProgress.start()
+  }
+  next()
+})
+
+router.afterEach((to, from) => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
 })

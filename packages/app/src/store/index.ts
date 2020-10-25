babc7f83c571b32d711012c6a21673a2f6ede50b
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:45:03 +0300

    feat(store*): add modules to store

--- packages/app/src/store/index.ts
+++ packages/app/src/store/index.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from '@/store/auth'
+import users from '@/store/users'
+import clients from '@/store/clients'
+import orders from '@/store/orders'
 import createPersistedState from 'vuex-persistedstate'
 
 import { getModule } from 'vuex-module-decorators'
@@ -15,10 +18,16 @@ export const store = new Vuex.Store({
   state: {},
   modules: {
     auth,
+    users,
+    clients,
+    orders,
   },
   plugins: [persistedState],
 })
 
 export const authModule = getModule(auth, store)
+export const usersModule = getModule(users, store)
+export const clientsModule = getModule(clients, store)
+export const ordersModule = getModule(orders, store)
 
 export default store

3a207a66dc50e843e34286024cd19671169c6089
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:42:51 +0300

    feat(notifications): init notifications plugin

---
+++ packages/app/src/plugins/notifications.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueNotification from '@kugatsu/vuenotification'
+import { TimelineMax } from 'gsap'
+import { NotificationPluginOptions } from '@/typings/misc/NotificationsPlugin'
+
+Vue.use(VueNotification, {
+  timer: 7,
+  showCloseIcn: true,
+  animateIn: function () {
+    const el = this.notificationEl
+    const tl = new TimelineMax()
+
+    tl.from(el, 0.4, {
+      left: 999,
+      width: 0,
+      position: 'absolute',
+    })
+
+    return tl
+  },
+  animateOut: function () {
+    const el = this.notificationEl
+    const tl = new TimelineMax()
+    tl.to(el, 0.5, {
+      opacity: 0,
+    })
+
+    return tl
+  },
+} as NotificationPluginOptions)

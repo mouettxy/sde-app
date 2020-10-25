c4d2084a17897cb459c21b845163a55d7dd555a9
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:46:07 +0300

    feat(api): init selects list

---
+++ packages/app/src/api/formSelectFields.ts
@@ -0,0 +1,49 @@
+export const userRolesSelect = [
+  { text: 'Курьер', value: 'expeditor' },
+  { text: 'Логист', value: 'logist' },
+  { text: 'Менеджер', value: 'manager' },
+  { text: 'Администратор', value: 'administrator' },
+]
+
+export const regionsSelect = [
+  { text: 'Краснодар', value: 'Краснодар' },
+  { text: 'Симферополь', value: 'Симферополь' },
+  { text: 'Севастополь', value: 'Севастополь' },
+  { text: 'Ставрополь', value: 'Ставрополь' },
+]
+
+export const userTransportSelect = [
+  { text: 'Мото', value: 'moto' },
+  { text: 'Авто', value: 'car' },
+]
+
+export const userPermissionsSelect = [
+  { text: 'Первый', value: 1 },
+  { text: 'Второй', value: 2 },
+  { text: 'Третий', value: 3 },
+]
+
+export const paymentTypeSelect = [
+  { text: 'Не указано', value: 'Не указано' },
+  { text: 'Нет отсрочки', value: 'Нет отсрочки' },
+  { text: 'Раз в неделю', value: 'Раз в неделю' },
+]
+export const paymentWhoSelect = [
+  { text: 'Не указано', value: 'Не указано' },
+  { text: 'Отправитель', value: 'Отправитель' },
+  { text: 'Получатель', value: 'Получатель' },
+  { text: 'Заказчик', value: 'Заказчик' },
+  { text: 'По разному', value: 'По разному' },
+]
+export const paymentFormSelect = [
+  { text: 'Не указано', value: 'Не указано' },
+  { text: 'Карта или наличные', value: 'Карта или наличные' },
+  { text: 'Счет', value: 'Счет' },
+]
+
+export const statusSelect = [
+  { text: 'Новая', value: 'Новая' },
+  { text: 'В работе', value: 'В работе' },
+  { text: 'Закрыта', value: 'Закрыта' },
+  { text: 'Не состоялась', value: 'Не состоялась' },
+]

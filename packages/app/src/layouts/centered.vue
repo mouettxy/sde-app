968920e3a46ff144e44795255be04ea324589c5c
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:45:39 +0300

    feat(layouts): add centered layout

---
+++ packages/app/src/layouts/centered.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.layout-centered
+  router-view
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class LayoutCentered extends Vue {}
+</script>
+
+<style lang="sass">
+.layout-centered
+  position: relative
+  display: flex
+  height: 100%
+  align-content: center
+  justify-content: center
+</style>

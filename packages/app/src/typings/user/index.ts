c10b7e63a5b3404eab5882292725f14ea4468ba4
newfox79 <znaykaznaykovich@gmail.com>
2020-10-25 23:40:30 +0300

    feat(typings): init user types

---
+++ packages/app/src/typings/user/index.ts
@@ -0,0 +1,39 @@
+export type UserStateBreak = {
+  onBreak: boolean
+  startTime: Date
+  endTime: Date
+}
+
+export type UserStateOrders = {
+  order: string
+  onPause: boolean
+  takeAt: Date
+}
+
+export type UserStateRoute = {}
+
+export type UserState = {
+  break: UserStateBreak
+  orders: UserStateOrders
+  route: UserStateRoute
+}
+
+export type User = {
+  _id?: string
+  username: string
+  password: string
+  comment: string
+  phone: string
+  email: string
+  credentials: string
+  active: boolean
+  canTakeOrders: boolean
+  workTime: string
+  role: string
+  transport: string
+  region: string
+  permissionsLevel: number
+  isSdeTransport: boolean
+  lastActive: string
+  state: UserState
+}
